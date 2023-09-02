1861b60457d9ee351a9b7abf5ac2518ce0b83de3
Anton <antontokmakov757@gmail.com>
2023-09-02 12:47:21 +0700

    Add full solution

--- FirasTask/Program.cs
+++ FirasTask/Program.cs
@@ -1,3 +1,30 @@
-﻿//Короткое решение
+﻿string[] masInput = Console.ReadLine().Split(" ");
+Console.WriteLine(string.Join(" ", MasChooseItem(masInput)));
+
+string[] MasChooseItem(string[] mas)
+{
+    int index = 0;
+    int sizeMasItem = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i].Length <= 3)
+        {
+            sizeMasItem++;
+        }
+    }
+    string[] masItem = new string[sizeMasItem];
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i].Length <= 3)
+        {
+            masItem[index] = mas[i];
+            index++;
+        }
+    }
+    return masItem;
+}
+
+/*Короткое решение
 string[] result = Console.ReadLine().Split(" ").Where(x => x.Length <= 3).ToArray();
 Console.WriteLine(string.Join(" ", result));
+*/
